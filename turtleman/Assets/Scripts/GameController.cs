83157bb6db3e1708821a61464fb6a3e609b97486
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-11-16 10:39:43 +0000

    Procedural maze, needs to be adapted but works as proof of concept

---
+++ turtleman/Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]               // 1
+
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor generator;
+
+    void Start()
+    {
+        generator = GetComponent<MazeConstructor>();      // 2
+        generator.GenerateNewMaze(13, 15);
+    }
+}
