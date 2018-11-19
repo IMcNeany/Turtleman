6975b9db88d38b2b7b078f22e861f0cfaec86292
Unknown <nathan@thebarnetts.co.uk>
2018-11-19 02:37:05 +0000

    basic movement

---
+++ turtleman/Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject player;
+    public float CameraHeight;
+
+
+    private Vector3 offset;        
+    void Start()
+    {
+        offset = new Vector3(0,CameraHeight ,transform.position.z - player.transform.position.z);
+    }
+    
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform);
+    }
+}
---
+++ turtleman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal_1") * Time.deltaTime * 100.0f;
+        var z = Input.GetAxis("Vertical_1") * Time.deltaTime * 2.0f;
+        anim.SetFloat("Horizontal", (x * 10));
+        anim.SetFloat("Vertical", (z * 10));
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
