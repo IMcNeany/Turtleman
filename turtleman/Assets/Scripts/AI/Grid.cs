ce74521b7c08ae6d4da4675b2aface1ec71a49d5
SamHibb <samhibb16@gmail.com>
2018-11-19 16:52:12 +0000

    Adding some test scenes for AI

---
+++ turtleman/Assets/Scripts/AI/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+	public class Node
+	{
+		int gCost;
+		int hCost;
+		float x, y;
+		float gridX, gridY;
+
+		Node parent;
+
+		int calculateFCost() { return gCost + hCost; }
+	}
+
+	private Node[] nodes;
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
