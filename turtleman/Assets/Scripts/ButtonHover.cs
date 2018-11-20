e4540ccdfa888587a7c7d6aee4392cf33ede33c1
Patrick Charleson <spudcharleson@hotmail.co.uk>
2018-11-20 19:37:07 +0000

    Added Audio to Menu and Buttons. WARNING: The audio was so loud that I lost whatever hearing I had left. Also, sounds are temporary

---
+++ turtleman/Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHover : MonoBehaviour {
+
+    public AudioSource audioSource1;
+    public AudioClip hover;
+    public AudioClip click;
+
+    // Use this for initialization
+    public void HoverSound()
+    {
+        audioSource1.PlayOneShot(hover);
+    }
+
+    public void ClickSound()
+    {
+        audioSource1.PlayOneShot(click);
+    }
+}
