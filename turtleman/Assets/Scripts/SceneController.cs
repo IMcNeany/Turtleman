3fb7925ddcf2aafaf0b440ce2b397a55760b69e3
Patrick Charleson <spudcharleson@hotmail.co.uk>
2018-11-20 18:32:47 +0000

    Menu v1.1.0.3.7 beta Things needed: Shit to reset on game over A nice logo The game

---
+++ turtleman/Assets/Scripts/SceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private GameObject game_controller;
+
+    private void Start()
+    {
+        game_controller = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    private void ResetPassingData()
+    {
+        //game_controller.GetComponent<DataPersistance>().FlushData();
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void MenuScene()
+    {
+       ResetPassingData();
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
