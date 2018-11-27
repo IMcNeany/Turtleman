66a778d4b1e269fd43759dd877a9e603407a61e9
Unknown <nathan@thebarnetts.co.uk>
2018-11-27 12:06:59 +0000

    Basic version of the cutscene

---
+++ turtleman/Assets/Scripts/CameraLerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraLerp : MonoBehaviour {
+
+    public Transform target;
+    public Transform startPos;
+    public float travelTIme;
+    public Transform egg;
+    Vector3 start;
+    Vector3 end;
+    float timer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        start = startPos.position;
+        end = target.position;
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        transform.position = Vector3.Lerp(start, end, timer / travelTIme);
+        transform.LookAt(egg);
+	}
+}
