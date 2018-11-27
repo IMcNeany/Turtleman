96b684cda373a246ff3828d32b067b634a3a51a8
Isobel McNeany <icmc112@yahoo.co.uk>
2018-11-27 16:09:39 +0000

    Arrow to point in direction of egg

---
+++ turtleman/Assets/EggPointer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggPointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject egg = GameObject.Find("Egg(Clone)");
+        if (egg != null)
+        {
+            gameObject.transform.LookAt(egg.transform);
+        }
+	}
+}
