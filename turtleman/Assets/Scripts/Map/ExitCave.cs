6284671f3505fea6873125627427c2d5a1a8e5b2
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-11-27 18:59:20 +0000

    Eggs position fix + reload level when exit

--- turtleman/Assets/Scripts/Map/ExitCave.cs
+++ turtleman/Assets/Scripts/Map/ExitCave.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitCave : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class ExitCave : MonoBehaviour
         {
             //Exit to whatever stage
             Debug.Log("Exit Stage with " + ui.EggCount + " egg(s).");
+            SceneManager.LoadScene(2);
         }
     }
 }
