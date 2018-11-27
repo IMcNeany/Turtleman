a2f2f95b4026a3a0d25dda419be8755cf6011405
Patrick Charleson <spudcharleson@hotmail.co.uk>
2018-11-27 01:57:13 +0000

    Minor Edits to Main Menu, Game Over Scene. Working on High score in the morning

---
+++ turtleman/Assets/highScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highScore : MonoBehaviour
+{
+    public Text hiScoreText;
+    private UI_Manager eggScore;
+    public int newEggScore = 10;
+
+    private void Start()
+    {
+        eggScore = GetComponent<UI_Manager>();
+    }
+
+    private void Update()
+    {
+
+    }
+}
